90fa4396ed6a8651a65b00bca983ca3ee3ad8d63
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-11-28 14:58:39 -0500

    Suma de los valores uno a uno de las matrices

--- Cortez_Enzo_Suma_Matrices.c
+++ Cortez_Enzo_Suma_Matrices.c
@@ -38,7 +38,15 @@ int main(){
         }
     }
 
-    
+    /*Suma de matrices*/
+
+     for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrizC[i][j] = matrizA[i][j]+matrizB[i][j];
+        }
+    }
 
 }
 
