b55864ee99152c57d1a19b17b7cad612657d75ed
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-11-28 14:56:29 -0500

    Solicitud de dimensiones y correcion de sintaxis

--- Cortez_Enzo_Suma_Matrices.c
+++ Cortez_Enzo_Suma_Matrices.c
@@ -5,14 +5,22 @@
 
 
 int main(){
+    int m,n;
+
+    /*Solicitud de las dimesiones de las matrices*/
+    printf("Ingrese las dimensiones de la matriz");
+    printf("Ingrese el numero de filas");
+    scanf("%d",&m);
+    printf("Ingrese el numero de columnas");
+    scanf("%d",&n);
     /*Definir las matrices*/
-    int m,n; 
+    
     int matrizA[m][n];
     int matrizB[m][n];
     int matrizC[m][n];
 
     srand(time(0));
-
+    /*Generacion de los numeros dentro de la matriz A*/
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
@@ -21,7 +29,16 @@ int main(){
         }
     }
 
+    /*Generacion de los numeros dentro de la matriz B*/
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrizB[i][j] = rand() % 100 + 1;
+        }
+    }
 
+    
 
 }
 
